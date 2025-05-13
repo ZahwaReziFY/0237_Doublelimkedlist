207bfd16ad933137a5ab4d0eaa14ecf1d2fbd32f
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-13 10:35:05 +0700

    add while jika current not null

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -47,5 +47,11 @@ void addNode()
     {
         Node *current = START;
         Node *previous = NULL;
+
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        {
+            previous = current;
+            current = current->next;
+        }
     }
 }
