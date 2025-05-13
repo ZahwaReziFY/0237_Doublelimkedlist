2c02293ba851b0b08e86b0cf30a0c98c1695bc87
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-13 10:41:51 +0700

    add if current not null

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -57,6 +57,9 @@ void addNode()
         newNode->next = current;
         newNode->prev = previous;
 
-        
+        if (current != NULL)
+        {
+            current->prev = newNode;
+        }
     }
 }
