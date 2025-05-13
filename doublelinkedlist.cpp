77e61d72bc9fec4bbf3f45ca15d66f123f5975c0
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-13 10:15:31 +0700

    display if start not null

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -30,5 +30,10 @@ void addNode()
     if (START == NULL || newNode->noMhs <= START->noMhs)
     {
       if (START != NULL && newNode->noMhs == START-> noMhs)  
+      {
+        cout << "\033[31mDumplicated roll numbers not allowed\033]0m" << endl;
+        return;
+      }
+      
     }
 }
