21f97a81b707904037e0d93ac87351cf6b8b0d12
ZahwaReziFY <zahwarezi416@gmail.com>
2025-05-13 10:36:07 +0700

    add node baru

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -53,5 +53,10 @@ void addNode()
             previous = current;
             current = current->next;
         }
+
+        newNode->next = current;
+        newNode->prev = previous;
+
+        
     }
 }
